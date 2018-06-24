ef816621ecead2123adb86ba3a062c76c469e76c
ipriver <g1ran1q@gmail.com>
2018-06-24 18:27:58 +0300

    update sh

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Oh-my-zsh install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 DIRECTORY="$HOME/.config/alacritty"
 
