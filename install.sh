08739c50819a840c38565757055a9195ab261702
ipriver <g1ran1q@gmail.com>
2018-06-24 19:42:11 +0300

    updates

--- install.sh
+++ install.sh
@@ -11,12 +11,17 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 DIRECTORY="$HOME/.config/alacritty"
-
 if [ ! -d "$DIRECTORY" ]; then
-	mkdir -pv "$DIRECTORY"
+	mkdir -p "$DIRECTORY"
 fi
-
 cp alacritty.yml $DIRECTORY
+
+DIRECTORY="$HOME/.tmux"
+if [ ! -d "$DIRECTORY" ]; then
+	mkdir -p "$DIRECTORY"
+fi
+cp .tmux.conf $DIRECOTRY
+
 cp .vimrc $HOME
 
 vim -c ":PlugInstall" -c ":qa"
