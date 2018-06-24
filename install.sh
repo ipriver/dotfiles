8d83e36040f1904cbbe411ad3e56f2b0b3c33bae
ipriver <g1ran1q@gmail.com>
2018-06-24 19:43:02 +0300

    sh upd

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Plug-vim for vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# Plug-vim for neovim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 # Oh-my-zsh install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
