aea08fbca086c788c822ce59dca61045728c2921
ipriver <g1ran1q@gmail.com>
2018-06-24 15:11:34 +0000

    updates

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Plug-vim for vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Plug-vim for neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
 DIRECTORY="$HOME/.config/alacritty"
 
 if [ ! -d "$DIRECTORY" ]; then
@@ -8,3 +16,5 @@ fi
 
 cp alacritty.yml $DIRECTORY
 cp .vimrc $HOME
+
+vim -c ":PlugInstall" -c ":qa"
